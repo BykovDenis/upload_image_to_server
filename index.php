d83b955c6e4e2e052271747e5f3523948d7e3b8c
Bykov Denis <stivenrain@gmail.com>
2016-06-06 08:39:40 +0300

    Обработка файлов, подготовка передачи файлов на сервер

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charstet="utf-8">
+	<title>Загрузка фото на сервер</title>
+	<link href="css/styles.css" rel="stylesheet">
+</head>
+<body>
+	<main class="upload-photo">
+		<div class="upload-photo__layout clearfix">			
+			<div class="upload-photo__form">
+				<h1 class="upload-photo__header">Добавить фото на сайт</h1>
+				<form action="index.php" name="frmForm1" method="post" id="frmUpload" enctype="multipart/form-data">		
+				  <input  type="hidden" name="MAX_FILE_SIZE" value="50000000"  />		
+					<input class="btn"  type="submit" id="sbmUpload" name="sbmUpload" value="Загрузить фото">					
+					<label for="files1" class="upload-photo__file-label">Выбрать файлы</label>
+					<input class="btn btn--upload" type="file" name="file[]" id="files1" multiple>
+					<h3 class="upload-photo__count"></h3>
+					<div class="upload-photo__preview">
+					</div>
+				</form>	
+			</div>
+			<div id="img"></div>
+		</div>
+	</main>
+	
+	<script src="js/script.js">
+	</script>	
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,3 +1,200 @@
+var photo_preview = document.querySelector('.upload-photo__preview');
+var counter_photo = document.querySelector('.upload-photo__count');
+var arr_photo = []; // фото товаров
+
+var max_width = 800;
+var max_height = 800;
+
+var debug = 1;
+var formData = new FormData();
+
+// Загрузка данных 			
+
+document.querySelector('#files1').addEventListener('change', function(){
+
+  var files = this.files;
+
+  counter_photo.innerText = 'Выбрано '+files.length+' файлов';
+  
+  var files = this.files;
+  var length = files.length;
+  var size = files.size;  
+  var counter = 0;
+  
+   
+  for(var i=0; i< length; i++){
+
+      file = files[i];
+    
+      preview(file);
+    
+      var fr = new FileReader();        
+    
+      fr.addEventListener('load', function(event){
+
+        counter++;
+        var img = document.createElement('img');
+        img.src = event.target.result;
+
+        var img1 = document.createElement('img');
+        document.forms[0].appendChild(img1);
+
+        var imgScale = scaleImage(img);                 
+        //img1.src = imgScale;        
+
+        formData.append(files[counter-1].name, imgScale);// масштабированное изображение
+
+        if(debug){  
+          console.log('Имя файла : '+file.name);
+          console.log('Тип файла : '+file.type);
+          console.log('Размер файла : '+file.size+' bytes');              
+          console.log('Ширина картинки '+img.width);
+          console.log('Высота картинки '+img.height);     
+          console.log('Новая ширина картинки '+img1.width);
+          console.log('Новая высота картинки '+img1.height);   
+          console.log('Счетчик '+counter);
+
+          }
+          
+      },false);
+
+      fr.readAsDataURL(file);      
+
+  }       
+  
+});
+
+function preview(file){
+
+  // проверка на то что файлы изображения
+  if(file.type.match(/image.*/)){
+
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function(){
+
+      var divs = document.createElement('div');
+      divs.class = 'upload-photo__contain';
+
+      var img = document.createElement('img');
+        img.src = event.target.result; 
+        img.alt = file.name;
+        img.width = '250';
+
+      var span = document.createElement('span');
+        span.innerText = file.name;
+
+      photo_preview.appendChild(divs);
+      divs.appendChild(span);
+      divs.appendChild(img);	
+
+      arr_photo.push({file: file, img: img});
+
+
+    });	
+
+
+      reader.readAsDataURL(file);						
+
+  }
+
+}
+
+
+function dataPreparation(){
+  
+  console.log("Вызов dataPreparation");
+    
+  for(var key of formData.keys()){  
+
+    if(debug){            
+      console.log('Вызов');    
+      console.log('Ключ '+key);  
+      console.log('Изображение', formData.get(key));
+    }
+      sendToServer(formData.get(key));
+  } 
+  
+}
+
+/**
+ * Отправлять данные на сервер
+ * @param {[image]} img Изображение
+ */
+function sendToServer(img){
+  
+  console.log("Передача на сервер");
+  
+  var xhr = new XMLHttpRequest();  
+  
+  xhr.addEventListener('load', function(){
+    
+    div = document.getElementById('img');
+    var el = document.createElement('div');
+    div.appendChild(el);
+    el.innerHTML = xhr.responseText;
+    el.className = 'container__preview';   
+    
+  });
+  
+  xhr.open('POST','lib/response.php',true);  
+  xhr.setRequestHeader('Content-type', 'application/x-www-urlencoded');
+  xhr.send(img);
+  
+}
+
+
+function scaleImage(img){
+  
+  var canvas = document.createElement('canvas');
+
+  var ctx = canvas.getContext('2d');  
+  
+  if(img.width > max_width  || img.height > max_height){
+    
+    if(img.width > img.height){
+      
+      canvas.height = img.height * max_width/ img.width;
+      canvas.width = max_width;
+      
+    }
+    else{
+      
+      canvas.width = img.width * max_height / img.height;
+      canvas.height = max_height;
+      
+    }
+    
+  }
+  else{
+    
+    canvas.width = img.width;
+    canvas.height = img.height;
+    
+  }   
+  
+    console.log(img.width);
+    console.log(img.height);
+
+  ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  //document.forms[0].appendChild(canvas); 
+  
+  return canvas.toDataURL();
+    
+}
+
+document.getElementById("sbmUpload").addEventListener("click", function(){
+   
+   if(formData.keys()){
+     
+     dataPreparation();
+     
+   }                                                   
+                                                      
+},false);
+
+
+/*
 function JPEG(fileId, canvasId, debug){    
   
   this.descr = document.getElementById(fileId);  
@@ -12,7 +209,7 @@ function JPEG(fileId, canvasId, debug){
   
   if(!fileAPISupport) {
     
-    throw new Error("Браузер не поддерживает работу с API. Неободимо обновить браузер");
+    throw new Error('Браузер не поддерживает работу с API. Неободимо обновить браузер');
     return false;
     
   }
@@ -75,9 +272,9 @@ JPEG.prototype.scaleImage = function(img){
 }
 
 
-var obj = new JPEG("file1", "canvas", 0);
+var obj = new JPEG('file1', 'canvas', 0);
 
-obj.descr.addEventListener("change", function(){
+obj.descr.addEventListener('change', function(){
 
   var files = this.files;
   var length = files.length;
@@ -141,6 +338,10 @@ obj.descr.addEventListener("change", function(){
   }     
   
 },false);
+
+*/
+			
+
 
 
 
